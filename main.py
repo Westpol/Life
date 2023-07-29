f5f82271f83fe128500a79eb1339cd5a4f768656
Westpol <81779508+Westpol@users.noreply.github.com>
2023-07-29 15:48:52 +0200

    Window Setup

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import pygame
+
+
+screen = None
+width = None
+height = None
+
+
+def window_init():
+    global screen, width, height
+    pygame.init()
+    screen = pygame.display.set_mode((1000, 1000))
+    pygame.display.set_caption("Life")
+    width, height = pygame.display.get_window_size()
+
+
+def main_loop():
+    while 1:
+        pass
 
 
 if __name__ == "__main__":
-    pass
+    window_init()
+    main_loop()
